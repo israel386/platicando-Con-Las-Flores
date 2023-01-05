76b5e532eb3956c7bd6ff9682de57ac738682808
israel386 <israel_delarosa@knights.ucf.edu>
2023-01-04 19:37:08 -0500

    updated price

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const all = document.getElementById('all');
+all.addEventListener('click', toggle);
+function toggle() {
+    const isChecked = all.checked;
+    Array.from(document.getElementsByTagName('input')).forEach(element => {
+        element.checked = isChecked;
+    });
+}
+Array.from(document.querySelectorAll('input:not(#all)')).forEach(element => {
+    element.addEventListener('click', uncheckAll);
+});
+function uncheckAll() {
+    all.checked = false
+}
